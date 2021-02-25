50696105b41069fc180cfd8279ad267e8e615b8b
Mumtozbek Akhmadjanov <52557963+mumtozee@users.noreply.github.com>
2021-02-25 23:49:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <deque>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <stack>
 #include <string>
 #include <unordered_map>
@@ -49,10 +50,9 @@ class Graph {
   }
 };
 
-struct Node {};
-
 struct Car {
   std::vector<std::pair<int, int>> paths_;
+  int full_time_;
 };
 
 enum class Color { WHITE, GRAY, BLACK };
@@ -77,16 +77,34 @@ void DFSUtil(const Graph& graph, int vertex, std::vector<Color>& color,
     } else if (color[curr] == Color::GRAY) {
       vStack.pop();
       color[curr] = Color::BLACK;
-      general_time += std::get<1>(graph[parent[curr]][curr]);
+      for (int i = 0; i < graph[parent[curr]].size(); ++i) {
+        if (std::get<0>(graph[parent[curr]][i]) == curr) {
+          general_time += std::get<1>(graph[parent[curr]][i]);
+          break;
+        }
+      }
     } else {
       vStack.pop();
     }
   }
 }
 
+int ComputeCarTime(const Car& car, const Graph& graph) {
+  int result = 0;
+  for (const auto& path : car.paths_) {
+    for (int i = 0; i < graph[path.first].size(); ++i) {
+      if (std::get<0>(graph[path.first][i]) == path.second) {
+        result += std::get<1>(graph[path.first][i]);
+        break;
+      }
+    }
+  }
+  return result;
+}
+
 int main() {
   std::cout << "Enter data set name: \n";
-  std::string filename = "";
+  std::string filename;
   std::cin >> filename;
   int sim_duration = 0;
   int intersection_num = 0;
@@ -109,25 +127,38 @@ int main() {
   std::unordered_map<std::string, std::tuple<int, int, int>> city(
       num_of_streets);
 
+  std::vector<int> input_flow(intersection_num, 0);
+
   for (int i = 0; i < num_of_streets; ++i) {
     input_file >> a >> b >> street_name >> time;
+    std::cout << street_name << '\n';
     graph.addEdge(a, b, time, street_name);
     city[street_name] = std::tie(a, b, time);
+    ++input_flow[b];
   }
 
   std::vector<Car> cars(num_of_cars);
   Car car;
   int path_size = 0;
+
   for (int i = 0; i < num_of_cars; ++i) {
     input_file >> path_size;
     for (int j = 0; j < path_size; ++j) {
       input_file >> street_name;
       car.paths_.emplace_back(std::get<0>(city[street_name]),
                               std::get<1>(city[street_name]));
-      int cur_time = 0;
-
+      car.full_time_ = ComputeCarTime(car, graph);
+      if (car.full_time_ <= sim_duration) {
+        cars[i] = car;
+      }
     }
   }
+
+  auto compare = [](const Car& lhs, const Car& rhs) {
+    return lhs.full_time_ < rhs.full_time_;
+  };
+
+  std::set<Car, decltype(compare)> sorted_cars(compare);
 
   return 0;
 }
